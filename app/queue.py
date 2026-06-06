849a937d3b8a18a4cfacfb84bd8a0daf35272ca5
sakshammverma <sakshamverma124@gmail.com>
2026-06-07 02:21:23 +0530

    fix: redis connection pool to prevent max connections error

--- app/queue.py
+++ app/queue.py
@@ -7,19 +7,23 @@ load_dotenv()
 
 redis_url = os.getenv("REDIS_URL")
 if redis_url is None:
-    raise ValueError("REDIS_URL environment variable is required")
+    raise EnvironmentError("REDIS_URL is not set")
 
-client = redis.from_url(
+pool = redis.ConnectionPool.from_url(
     redis_url,
     ssl_cert_reqs=None,
-    decode_responses=True)
+    decode_responses=True,
+    max_connections=5
+)
 
-QUEUE_NAME = "pr-review-jbs"
+client = redis.Redis(connection_pool=pool)
 
-def enqueue_jb(job: dict):
+QUEUE_NAME = "pr_review_jobs"
+
+def enqueue_job(job: dict):
     client.lpush(QUEUE_NAME, json.dumps(job))
 
-def dequeue_jb():
+def dequeue_job():
     try:
         job = client.brpop(QUEUE_NAME, timeout=5)
         if job:
